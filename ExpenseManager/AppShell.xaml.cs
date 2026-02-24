6b70db7179ac8c7d292dba5441124e411faa288b
NazariyRomanyuk <naz.romanyuk@gmail.com>
2026-02-24 22:00:12 +0200

    Lab 2: add unversioned files

---
+++ ExpenseManager/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseManager;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ExpenseManager/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using ExpenseManager.Services;
+using Microsoft.Extensions.Logging;
+
+namespace ExpenseManager;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+        builder.Services.AddSingleton<IStorageService, StorageService>();
+        return builder.Build();
+    }
+}
